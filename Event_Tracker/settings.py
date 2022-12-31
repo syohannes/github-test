3acfe02db4d1079dbe22a6fefa9c729145f23ce2
Tiny Steps <yohannessharew55@gmail.com>
2022-12-31 08:17:05 -0300

    forms and user accounts

--- Event_Tracker/settings.py
+++ Event_Tracker/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'event.apps.EventConfig',
     'django.contrib.admin',
     'django.contrib.auth',
